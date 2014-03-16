c384bf7408ad07e9453724f3f1d79c639ba2864a
Ramsey D'silva <ramseydsilva@gmail.com>
2014-03-16 18:53:23 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+require.config({
+    paths: {
+        "jquery": "jquery-2.1.0.min",
+        "underscore": "underscore-min",
+        "bootstrap": "bootstrap.min",
+        "text": "plugins/text"
+    },
+    shim: {
+        "bootstrap": ["jquery"]
+    }
+});
+
+define([
+], function(){
+});
